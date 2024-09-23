6afbc1dd1e4fa4f0a64e52ccfa2e0dee711c3792
Eric Stiens <estiens@users.noreply.github.com>
2024-09-23 18:18:38 -0500

    change model

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ st.set_page_config(page_title='Magic NVC helper!', page_icon=':robot:')
 
 
 def load_chain():
-    llm = ChatOpenAI(model_name="gpt-4-turbo-preview", temperature=0.4)
+    llm = ChatOpenAI(model_name="chatgpt-4o-latest", temperature=0.5)
     if "conversation_summary" in st.session_state:
         st.session_state["conversation_summary"].clear()
     else:
